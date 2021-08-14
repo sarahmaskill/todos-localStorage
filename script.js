e6216263f3d8c5b4c79ba06e16fe1d5f3e67401d
Sarah Maskill <sarahnmaskill@gmail.com>
2021-08-14 12:55:31 -0400

    added styles

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hello")
+localStorage.setItem("myName", "Sarah")
+localStorage.setItem("petName","Sophie")
+localStorage.setItem("petAge",3)
+
+console.log(localStorage.getItem("myName"))
+
+var myName = localStorage.getItem('myName')
+var petName = localStorage.getItem("petName")
+var petAge = localStorage.getItem("petAge")
+
+console.log(myName, petName, petAge)
+
+
